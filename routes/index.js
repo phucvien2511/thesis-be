4f51cfafdfa42842d1c2288be76d42d79f8c9101
minhphucvien <vienphuc2511@gmail.com>
2024-03-09 16:30:51 +0700

    feat: change data routes

--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,14 @@ const _BASE_URL = "/api";
 // Define routes here
 const dataRoutes = require('./_dataRoutes');
 const deviceRoutes = require('./_deviceRoutes');
-const userRoutes = require('./_userRoutes');
+// const userRoutes = require('./_userRoutes');
 const topicRoutes = require('./_topicRoutes');
 const roomRoutes = require('./_roomRoutes');
 
 // Route configuration
-router.use(_BASE_URL + "/topic", dataRoutes);
+router.use(_BASE_URL + "/data", dataRoutes);
 router.use(_BASE_URL + "/device", deviceRoutes);
-router.use(_BASE_URL + "/user", userRoutes);
+// router.use(_BASE_URL + "/user", userRoutes);
 router.use(_BASE_URL + "/topic", topicRoutes);
 router.use(_BASE_URL + "/room", roomRoutes);
 
